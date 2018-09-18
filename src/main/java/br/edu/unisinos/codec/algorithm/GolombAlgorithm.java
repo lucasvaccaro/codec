1ed9fed583aea2da92a5edaf53d48062ab38c791
guibarth <guilherme_barth96@hotmail.com>
2018-09-17 21:33:08 -0300

    Adding GolombAlgorithm class  - code to treat negatives value to be added

---
+++ src/main/java/br/edu/unisinos/codec/algorithm/GolombAlgorithm.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.unisinos.codec.algorithm;
+
+/**
+ *
+ * @author I852885
+ */
+public class GolombAlgorithm extends Algorithm {
+    
+    public int[] positiveDecimals;
+    
+    public GolombAlgorithm(){
+        super();
+        positiveDecimals = new int[this.input.length];
+    }
+    
+    private void getPositiveDecimals(){
+        //I will write this later
+    }
+    
+    
+    
+}
