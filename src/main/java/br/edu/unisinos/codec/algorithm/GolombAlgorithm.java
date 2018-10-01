aca47a65fbaa1f1e6cedbd4702a74575dc2874db
guibarth <guilherme_barth96@hotmail.com>
2018-10-01 16:30:39 -0300

    Golomb algorithm working as long as LZ uses window of 127

--- src/main/java/br/edu/unisinos/codec/algorithm/GolombAlgorithm.java
+++ src/main/java/br/edu/unisinos/codec/algorithm/GolombAlgorithm.java
@@ -15,7 +15,8 @@ public class GolombAlgorithm extends Algorithm {
     }
 
 	public void compress() {
-		this.getPositiveDecimals();
+                positiveDecimals = new int[this.input.length];
+		this.getIntValues();
 		this.compressed_string = new String("");
 		
 		// add divider
@@ -25,52 +26,51 @@ public class GolombAlgorithm extends Algorithm {
 		int sufix;
 
 		for (int i = 0; i < this.positiveDecimals.length; i++) {
+                    if(i == positiveDecimals.length -2){
+                        int aaaaaa= 0;
+                    }
 			prefix = this.positiveDecimals[i] / DIV;
 			sufix = this.positiveDecimals[i] % DIV;
 			this.compressed_string = this.compressed_string.concat(getSequence(prefix, sufix, DIV));
 		}
-		
-		this.output = new byte[(int) Math.ceil((double)this.compressed_string.length()/8.0)];
-        byte b = 0xf;
-        int bytes_i = 0;
-        int j = 7;
-        for (int i = 0; i < this.compressed_string.length(); i++) {
-        	if (this.compressed_string.charAt(i) == '0') {
-        		b &= ~(1 << j);
-        	} else {
-        		b |= 1 << j;
-        	}
-        	if (j > 0) {
-        		j--;
-        	} else {
-        		j = 7;
-        		this.output[bytes_i] = b;
-        		bytes_i++;
-        		b = 0xf;
-        	}
-        }
+                
+                //create output with the proper size
+                int len = 0;
+                if(compressed_string.length()%8 != 0){
+                    len = compressed_string.length()/8 + 1;
+                }else{
+                    len = compressed_string.length()/8;
+                }
+                this.output = new byte[len];
+                
+                //convert int to byte for the output
+                for(int i = 0; i < this.output.length; i++){
+                    if(i == (this.output.length - 1)){
+                        this.output[i] = (byte) Integer.parseInt(compressed_string.substring(0), 2);
+                    }else{
+                        this.output[i] = (byte) Integer.parseInt(compressed_string.substring(0, 8), 2);
+                        compressed_string = compressed_string.substring(8);
+                    }
+                }
+                
 	}
 
 	public void decompress() {
-		this.compressed_string = new String("");
-		
-        for(int i = 0; i < this.input.length; i++){
-        	for(int k = 7; k >= 0; k--) {
-        		if (((this.input[i] >> k) & 1) == 1) {
-        			this.compressed_string = this.compressed_string.concat("1");
-        		} else {
-        			this.compressed_string = this.compressed_string.concat("0");
-        		}
-        	}
-        }
-        
+            
+            treatDecompInput();          
+            
+	
+                
 		int div = getDivider(this.compressed_string);
 		// take divider off the string to be analyzed
 		this.compressed_string = this.compressed_string.substring(5);
 
 		// calculate number
 		ArrayList<Byte> bytes = new ArrayList<Byte>();
-		while (this.compressed_string.length() > 7) {
+		while (this.compressed_string.length() > 0) {
+                    if(this.compressed_string.length() <=8){
+                        int aaaaa = 0;
+                    }
 			int prefix = getPrefix();
 			int sufix = getSufix();
 			int res = (prefix * div) + sufix;
@@ -98,7 +98,7 @@ public class GolombAlgorithm extends Algorithm {
 		} else {
 			binary_2bit = binary_xbit;
 		}
-		binary_2bit = formBinary(binary_2bit);
+		binary_2bit = formBinary4(binary_2bit);
 
 		result = result.concat(binary_2bit);
 		return result;
@@ -138,17 +138,39 @@ public class GolombAlgorithm extends Algorithm {
 	}
 
 	private int getSufix() {
-		String s = this.compressed_string.substring(0, 4);
-		this.compressed_string = this.compressed_string.substring(4);
+                String s = new String("");
+                if(this.compressed_string.length() < 4){
+                    s = formBinary4(this.compressed_string);
+                    this.compressed_string = new String();
+                }else{
+                    s = this.compressed_string.substring(0, 4);
+                    this.compressed_string = this.compressed_string.substring(4);
+                }		
 		int sufix = Integer.parseUnsignedInt(s, 2);
 		return sufix;
 	}
 
 	private String formBinary(String binary) {
 		String result = new String("");
+		if (binary.length() < 8) {
+			int dif = 8 - binary.length();
+			// add zeros to form 2 bits
+			for (int i = 0; i < dif; i++) {
+				result = result.concat("0");
+			}
+			result = result.concat(binary);
+		} else {
+			result = binary;
+		}
+
+		return result;
+	}
+        
+        private String formBinary4(String binary) {
+		String result = new String("");
 		if (binary.length() < 4) {
 			int dif = 4 - binary.length();
-			// add zeros to form 2 bits
+			// add zeros to form 4 bits
 			for (int i = 0; i < dif; i++) {
 				result = result.concat("0");
 			}
@@ -182,5 +204,26 @@ public class GolombAlgorithm extends Algorithm {
 			}
 		}
 	}
+        
+        private void getIntValues(){
+            for(int i = 0; i < this.input.length; i++){
+                positiveDecimals[i] = (int) Integer.parseInt(Integer.toString((int)this.input[i]));
+            }
+        }
+        
+        private void treatDecompInput(){
+            compressed_string = new String("");
+            for(int i = 0; i<this.input.length; i++){
+                String bin = Integer.toBinaryString((int)this.input[i]);
+                if(bin.length() > 8){
+                    int dif = bin.length() - 8;
+                    bin = bin.substring(dif);
+                }else{
+                    if(i != (this.input.length - 1))
+                        bin = formBinary(bin);
+                }
+                compressed_string = compressed_string.concat(bin);
+            }
+        }
 
 }
